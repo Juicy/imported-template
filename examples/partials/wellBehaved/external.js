5c3af8a0b30f3eb8ce56d4090b59543d845b96c8
Tomek Wytrebowicz <tomalecpub@gmail.com>
2014-03-28 09:02:52 +0100

    Make model global in examples, add more descriptions

--- examples/partials/wellBehaved/external.js
+++ examples/partials/wellBehaved/external.js
@@ -1,1 +1,4 @@
-console.info("/partials/wellBehaved/external.js was executed in imported document", document.currentScript.ownerDocument);
+// `document.currentScript.ownerDocument` references this import's document
+console.info("/partials/wellBehaved/external.js was executed in imported document", 
+	document.currentScript.ownerDocument);
+// `document` references the main document (the page that's importing us)
